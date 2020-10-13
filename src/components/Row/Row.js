049514ce834e8fa1bb4e89be8f7c0d70d1e7b436
azizulbappy0096 <azizulbappy6@gmail.com>
2020-10-14 02:12:37 +0600

    Just set up initial files

---
+++ src/components/Row/Row.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Row = () => {
+     return (
+          <div className="row__posters">
+               <h1>Welcome to Netflix Clone</h1>
+          </div>
+     )
+}
+
+export default Row
